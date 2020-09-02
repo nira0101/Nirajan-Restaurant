2f73c2662e98c3767ef1b1217a8d543e26babf06
Nirajan <nira0101@stud.kea.dk>
2020-09-02 03:12:02 -0500

    added class

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-fetch("https://kea-alt-del.dk/t5/api/liveproductlist?category=starter")
+fetch("https://kea-alt-del.dk/t5/api/productlist")
          .then(function (response) {
              console.log(response)
              return response.json();
@@ -8,16 +8,28 @@ fetch("https://kea-alt-del.dk/t5/api/liveproductlist?category=starter")
          })
 
 function dataReceived(products) {
-    products.forEach(showProduct)
+
+    products.forEach(showProduct);
 }
 
 function showProduct(myProduct) {
-    console.log(myProduct)
+    console.log("mProduct")
+
     const temp = document.querySelector("#productTemplate").content;
     const myCopy = temp.cloneNode(true);
+
+    const article = myCopy.querySelector("article");
+    if(myProduct.vegeterian){
+          article.classList.add("vegeterian");
+    }
+
+
     myCopy.querySelector(".data_name").textContent = myProduct.name;
-     myCopy.querySelector(".descrip").textContent = myProduct.description;
      myCopy.querySelector(".data_price").textContent = myProduct.price;
     const parentElem = document.querySelector("section#starter");
-    parentElem.appendChild(myCopy)
-          }
+    parentElem.appendChild(myCopy);
+
+
+}
+
+
